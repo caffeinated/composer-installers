d95ea09e4d35310e1fb3859c7c6d44638049d785
Kai <shea.lewis89@gmail.com>
2021-03-24 14:42:32 -0700

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -15,4 +15,14 @@ class Plugin implements PluginInterface
         
         $composer->getInstallationManager()->addInstaller($installer);
     }
+    
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        //
+    }
+    
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        //
+    }
 }
